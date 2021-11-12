10e980dea489fd0c81ce818132061704a5a9d38e
ichapatricia <93314501+ichapatricia@users.noreply.github.com>
2021-11-12 09:08:13 +0700

    Add files via upload

---
+++ 1_E_71210784.py
@@ -0,0 +1,11 @@
+print ("01")
+alas_atas = int(input("masukan alas atas:"))
+alas_bawah = int(input(" masukan alast bawah:"))
+tinggi = int(input("masukan tinggi:"))
+luas_trapesium = ((alas_atas + alas_bawah)*tinggi)/2
+print ("luas trapesium adalah: " + str(luas_trapesium))
+
+
+                       
+
+
