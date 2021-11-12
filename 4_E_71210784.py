16d8a80f8179d1290473f08c8737fd82219ebeea
ichapatricia <93314501+ichapatricia@users.noreply.github.com>
2021-11-12 09:08:59 +0700

    Add files via upload

---
+++ 4_E_71210784.py
@@ -0,0 +1,6 @@
+a = int(input("masukkan suku pertama: "))
+r = int(input("masukkan rasio: "))
+n = int(input("masukkan suku yang dicari: "))
+
+sn = a*(r**n-1)/r-1
+print(" Jumlah suku ke 11 : ", sn)
